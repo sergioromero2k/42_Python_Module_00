d1f3313564d54d965651635a44e1c9f375b06702
Sergio Alejandro Romero López <sergioromero2k@gmail.com>
2026-01-22 21:20:58 +0100

    correctio of ft_harvest_recursive

--- ex5/ft_count_harvest_recursive.py
+++ ex5/ft_count_harvest_recursive.py
@@ -1,16 +1,46 @@
-def ft_count_harvest_iterative():
+def ft_count_harvest_recursive():
     """
     Counts days until harvest using a loop.
     "Encapsulation": The helper function doesn't exist outside the main
     function, so tenchnically the file only exports one
     function to the outside.
     """
-    until_day = int(input("Days until harvest: "))
+    if not hasattr(
+        ft_count_harvest_recursive,
+        "day"
+    ):
+        ft_count_harvest_recursive.day = 1
+        ft_count_harvest_recursive.until_day = int(
+            input("Days until harvest: ")
+        )
 
-    def helper(day):
-        if day > until_day:
-            print("Harvest time!")
-            return
-        print(f"Day {day}")
-        helper(day + 1)
-    helper(1)
+    if (
+        ft_count_harvest_recursive.day
+        > ft_count_harvest_recursive.until_day
+    ):
+        print("Harvest time!")
+        del ft_count_harvest_recursive.day
+        del ft_count_harvest_recursive.until_day
+        return
+
+    print(f"Day {ft_count_harvest_recursive.day}")
+    ft_count_harvest_recursive.day += 1
+    ft_count_harvest_recursive()
+
+
+# def ft_count_harvest_iterative():
+#     """
+#     Counts days until harvest using a loop.
+#     "Encapsulation": The helper function doesn't exist outside the main
+#     function, so tenchnically the file only exports one
+#     function to the outside.
+#     """
+#     until_day = int(input("Days until harvest: "))
+
+#     def helper(day):
+#         if day > until_day:
+#             print("Harvest time!")
+#             return
+#         print(f"Day {day}")
+#         helper(day + 1)
+#     helper(1)
