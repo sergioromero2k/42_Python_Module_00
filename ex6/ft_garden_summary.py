14c951fd10e0dc037b3e4f350c181ee04ac916c1
Sergio Alejandro <sergioromero2k@gmail.com>
2026-01-23 01:35:10 +0100

    flake8 add

--- ex6/ft_garden_summary.py
+++ ex6/ft_garden_summary.py
@@ -5,6 +5,6 @@ def ft_garden_summary():
     """
     enter_garden = input("Enter garden name: ")
     enter_plants = input("Enter number of plants: ")
-    print(f"Garden:: {enter_garden}")
+    print(f"Garden: {enter_garden}")
     print(f"Plants: {enter_plants}")
     print("Status: Growing well!")
